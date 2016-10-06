9c3f63b26171976f42e52238fb0fd5f293ad593e
David-Sanchez-UJA-student <dsf00005@red.ujaen.es>
2016-10-06 11:51:39 +0200

    6/10

--- app/src/main/java/com/example/dimanxe/prueba1/Autentication.java
+++ app/src/main/java/com/example/dimanxe/prueba1/Autentication.java
@@ -5,4 +5,40 @@ package com.example.dimanxe.prueba1;
  */
 
 public class Autentication {
+
+    public static final int SERVICE_PORT=6000;
+
+    protected String mUser="";
+    protected String mPass="";
+    protected String mIP="";
+    protected int mPort=SERVICE_PORT;
+
+
+    public Autentication(String user,String pass, String IP, int port){
+        this.mUser=user;
+        this.mPass=pass;
+        this.mIP=IP;
+        this.mPort=port;
+    }
+
+    public String getmUser(){
+        return mUser;
+    }
+    public void setmUser(String user){
+        this.mUser=user;
+    }
+    public String getmPass(){
+        return mPass;
+    }
+    public void setmPass(String pass){
+        mPass=pass;
+    }
+    public String getmIP(){
+        return mIP;
+    }
+    public void setmIP(String ip){
+        mIP=ip;
+    }
+
+
 }
