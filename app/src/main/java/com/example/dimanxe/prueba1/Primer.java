fee3cefa58c11bfb3b136fe2f79647d934151948
David-Sanchez-UJA-student <dsf00005@red.ujaen.es>
2016-10-11 20:21:58 +0200

    11/10

--- app/src/main/java/com/example/dimanxe/prueba1/Primer.java
+++ app/src/main/java/com/example/dimanxe/prueba1/Primer.java
@@ -1,5 +1,7 @@
 package com.example.dimanxe.prueba1;
 
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -9,5 +11,11 @@ public class Primer extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_primer);
+        FragmentManager fm = getSupportFragmentManager();
+        FragmentTransaction ft = fm.beginTransaction();
+        AuthFragment au = AuthFragment.newInstance("pepe","12345");
+        ft.add(R.id.main_frame,au);
+        ft.addToBackStack(null);
+        ft.commit();
     }
 }
